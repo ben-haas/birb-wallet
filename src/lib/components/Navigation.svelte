27f4055e22dbf83e6a326969251f98d13340cfab
Ben Haas <ben@benhaas.io>
2024-01-15 20:54:34 -0800

    Add wallet button

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { get } from 'svelte/store';
 	import { getDrawerStore, Avatar } from '@skeletonlabs/skeleton';
 	import { accountStore, activeWallet } from '$lib/stores';
+	import { Fa } from 'svelte-fa';
+	import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-	$: active = (id: string) => (id === $activeWallet.toString() ? '!bg-secondary-500' : '');
+	$: active = (id: string) => (id === $activeWallet.toString() ? '!bg-primary-500' : '');
 
 	const drawerStore = getDrawerStore();
 
@@ -13,15 +14,20 @@
 	}
 </script>
 
-<nav class="list-nav p-4 !bg-primary-100">
+<nav class="list-nav p-4">
 	<ul>
 		{#each $accountStore as acct}
 			<li>
 				<a href="/" class={active(acct.id.toString())} on:click={() => setActiveWallet(acct.id)}>
 					<span><Avatar initials="ꜩ" background="bg-tertiary-500" width="w-8" /></span>
-					<span>{acct.id}</span>
+					<span class="flex-auto">{acct.id}</span>
 				</a>
 			</li>
 		{/each}
 	</ul>
+	<div class="flex justify-center p-4">
+		<a href="/">
+			<Fa icon={faPlus} size="lg" />
+		</a>
+	</div>
 </nav>
