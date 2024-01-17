234ce81e98854f7a40e2303bc2b7ed1b852ae870
Ben Haas <ben@benhaas.io>
2024-01-16 21:24:57 -0800

    move tzkt api calls to lib

---
+++ src/lib/services/tzktService.ts
@@ -0,0 +1,58 @@
+export interface WalletData {
+	type: string;
+	address: string;
+	publicKey: string;
+	revealed: boolean;
+	alias: string;
+	balance: number;
+	delegate: {
+		alias: string;
+		address: string;
+		active: boolean;
+	};
+	firstActivityTime: string;
+	lastActivityTime: string;
+}
+
+export const getWalletData = async (address: string): Promise<WalletData> => {
+	const url = 'https://api.tzkt.io/v1/accounts?';
+
+	try {
+		const res = await fetch(
+			url +
+				new URLSearchParams({
+					address: address,
+					select:
+						'type,address,publicKey,revealed,alias,balance,delegate,firstActivityTime,lastActivityTime'
+				})
+		);
+
+		if (!res.ok) {
+			throw new Error('The TZKT API call returned an error response');
+		}
+
+		const data = await res.json();
+		return data[0];
+	} catch (error) {
+		console.error('There was a problem fetching the wallet data:', error);
+		throw error;
+	}
+};
+
+export const getCounter = async (address: string): Promise<number> => {
+	const url = `https://api.tzkt.io/v1/accounts/${address}/counter`;
+
+	try {
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error('The TZKT API call returned an error response');
+		}
+
+		const counter = await res.json();
+		return counter;
+	} catch (error) {
+		console.error('There was a problem fetching the counter:', error);
+		throw error;
+	}
+};
