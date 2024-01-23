95b8873aa399d75a9b846605351037649a0d24d2
Ben Haas <ben@benhaas.io>
2024-01-22 21:30:55 -0800

    add tailwind forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 import { birbTheme } from './birb_theme';
 
@@ -10,6 +11,7 @@ const config = {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	plugins: [
+		forms,
 		skeleton({
 			themes: {
 				custom: [birbTheme],
