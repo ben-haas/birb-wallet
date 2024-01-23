3f226b0ecf5bf13b876a7b157d9967dafa896aff
Ben Haas <ben@benhaas.io>
2024-01-22 21:32:18 -0800

    Add page for adding new accounts

---
+++ src/lib/components/ConnectWallet.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getAddress } from '$lib/services/trezorService';
+	import { accountStore } from '$lib/stores';
+
+	function connect(): void {
+		getAddress(1);
+	}
+
+	//TODO add input to choose derivation path
+</script>
+
+<div class="flex flex-col items-center space-y-4">
+	<h2 class="h2">Connect Your Trezor</h2>
+	<button class="btn variant-filled-primary" on:click={connect}>Connect</button>
+</div>
