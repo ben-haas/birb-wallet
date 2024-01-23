eb86402bbce27e82f284d50317734b2389f85ad2
Ben Haas <ben@benhaas.io>
2024-01-23 11:47:45 -0800

    connect page

---
+++ src/routes/connect/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import ConnectWallet from '$lib/components/ConnectWallet.svelte';
+</script>
+
+<div class="container h-full mx-auto gap-8 flex flex-col items-center">
+	<ConnectWallet />
+</div>
