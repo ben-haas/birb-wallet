de635939e5c30a4a60624e7b4cdb2427818594b2
Ben Haas <ben@benhaas.io>
2024-01-22 21:38:41 -0800

    remove trezor init and condition activeWallet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { trezorInit } from '$lib/trezor';
 	import { activeWallet, accountStore } from '$lib/stores';
 	import Wallet from '$lib/components/Wallet.svelte';
 
 	onMount(() => {
-		trezorInit();
-		activeWallet.set({ id: $accountStore[0].id, address: $accountStore[0].address });
+		if ($accountStore[0]) {
+			activeWallet.set({
+				id: $accountStore[0].id,
+				address: $accountStore[0].address,
+				loaded: true
+			});
+		}
 	});
 </script>
 
