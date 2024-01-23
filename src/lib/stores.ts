5e715f465a13996d74e7e0c28367b211963818cd
Ben Haas <ben@benhaas.io>
2024-01-23 11:45:08 -0800

    add writable for current quote

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -9,3 +9,4 @@ type Account = {
 
 export const accountStore: Writable<Account[]> = localStorageStore('accounts', []);
 export const activeWallet = writable({ id: 0, address: '', loaded: false });
+export const currentQuote = writable(0);
